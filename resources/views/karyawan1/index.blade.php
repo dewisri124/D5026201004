1d1ce4c9789a406b07c2fe4fbb591c5ab8b4387d
dewi sri wahyuni <90193365+dewisri124@users.noreply.github.com>
2021-12-15 14:23:36 +0700

    EAS fiks
    index.blade.php

--- resources/views/karyawan1/index.blade.php
+++ resources/views/karyawan1/index.blade.php
@@ -1,49 +1,36 @@
 @extends('layout.ceria')
 
-@section('title', 'Data karyawan1')
+@section('title', 'Data Karyawan')
 
 @section('content')
 
 <div class="container">
-	<h3 class="text-center">Data karyawan1</h3>
-	<a href="/karyawan1/tambah" class="btn btn-secondary"> + Tambah karyawan1 Baru</a>
+	<h3 class="text-center">Data Karyawan</h3>
+    <a href="/karyawan1/tambah" class="btn btn-secondary"> + Tambah Karyawan Baru</a>
 	<br/>
-	<div class="container" align="center">
-        <p>Cari Data karyawan1 Berdasarkan NIP :</p>
-	    <form action="/karyawan1/cari" method="GET">
-            <input class="form-control" type="text" name="cari" placeholder="Cari karyawan1 .." value="{{ old('cari') }}">
-            <input class="form-control btn-success" type="submit" value="CARI">
-	    </form>
-    </div>
+
 	<table border="1" class="table table-primary mt-3">
 		<tr>
-            <th>No</th>
-			<th>NIP</th>
-			<!--<th>nama</th>
-			<th>pangkat</th>-->
-			<th>gaji</th>
+            <th>NIP</th>
+			<th>nama</th>
+			<th>pangkat</th>
+            <th>gaji</th>
 			<th>Opsi</th>
 		</tr>
 
-		@foreach($karyawan1 as $p)
+		@foreach($karyawan1 as $k)
 
 		<tr>
-			<td>{{ $loop->iteration }}</td>
-            <td>{{ $p->karyawan1_NIP }}</td>
-			<!--<td>{{ $p->karyawan1_nama }}</td>
-			<td>{{ $p->karyawan1_pangkat }}</td> -->
-			<td>{{ $p->karyawan1_gaji }}</td>
+            <td>{{ $k->NIP }}</td>
+			<td>{{ $k->nama }}</td>
+			<td>{{ $k->pangkat }}</td>
+			<td>{{ number_format ($k->gaji) }}</td>
 			<td>
-                <a href="/karyawan1/detail/{{ $p->karyawan1_id }}" class="btn btn-info">View Detail</a>
-				<a href="/karyawan1/edit/{{ $p->pkaryawan1_id }}" class="btn btn-warning">Edit</a>
-				<a href="/karyawan1/hapus/{{ $p->karyawan1_id }}" class="btn btn-danger">Hapus</a>
+				<a href="/karyawan1/hapus/{{ $k->NIP }}" class="btn btn-danger">Hapus</a>
 			</td>
 		</tr>
 		@endforeach
 	</table>
-
-    {{ $karyawan1->links() }}
-
 </div>
 
 @endsection
